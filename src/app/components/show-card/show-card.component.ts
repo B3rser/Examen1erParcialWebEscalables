6eba4a2ba48baad866220fbf80661350e3858072
Marco Uriel López García <uriel.lops.jasjas@gmail.com>
2024-09-12 16:59:03 -0600

    Progress made in class :feelsgood:

---
+++ src/app/components/show-card/show-card.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Show } from '../../interfaces/show.interface';
+import { NgClass, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-show-card',
+  standalone: true,
+  imports: [NgIf, NgClass],
+  templateUrl: './show-card.component.html',
+  styleUrl: './show-card.component.css'
+})
+
+export class ShowCardComponent {
+  @Output()
+  public deleteCard: EventEmitter<string> = new EventEmitter();
+  
+  @Input()
+  public show: Show = {
+    name: "",
+    description: "",
+    image: "",
+    year: 0,
+    episodes: 0,
+    genre: "",
+    likes: [],
+  };
+
+  public isSelected: boolean = false;
+
+  public changeSelected(): void {
+    this.isSelected = !this.isSelected
+  }
+
+  public onDeleteCard(){
+    this.deleteCard.emit(this.show.name);
+  }
+}
---
+++ src/app/components/show-list/show-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ShowCardComponent } from "../show-card/show-card.component";
+import { NgFor } from '@angular/common';
+import { Show } from '../../interfaces/show.interface';
+
+@Component({
+  selector: 'app-show-list',
+  standalone: true,
+  imports: [ShowCardComponent, NgFor,],
+  templateUrl: './show-list.component.html',
+  styleUrl: './show-list.component.css'
+})
+
+export class ShowListComponent {
+  @Output()
+  public deleteCard: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public shows: Show[] = [];
+
+  public deleteElement(name: string): void {
+    this.deleteCard.emit(name);
+  }
+}
